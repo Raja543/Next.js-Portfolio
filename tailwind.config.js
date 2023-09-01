86200b3dceb815ffa96a9d67ef0ccc678fbd047f
Raja Kumar <rajakumarmahto952@gmail.com>
2023-09-02 01:38:15 +0530

    navbar completed

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+
+const fontFamily = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.fontFamily.sans],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 88,230,217
+      },
+      screens: {
+        "2xl": { max: "1535px" },
+        xl: { max: "1279px" },
+        lg: { max: "1023px" },
+        md: { max: "767px" },
+        sm: { max: "639px" },
+        xs: { max: "479px" },
+      },
+    },
+  },
+  plugins: [],
+};
